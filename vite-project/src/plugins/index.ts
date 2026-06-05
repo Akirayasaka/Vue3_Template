598c4669cecdb47dd4192505ccace5bbbfdfdf53
Akira <kuramal12@msn.com>
2026-06-05 10:23:52 +0800

    Update Packages, Remove vue-axios.

--- vite-project/src/plugins/index.ts
+++ vite-project/src/plugins/index.ts
@@ -1,6 +1,5 @@
 import { type App } from "vue";
 import axios from "axios";
-import VueAxios from "vue-axios";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import router from "@/router";
@@ -15,7 +14,6 @@ export const registerPlugins = (app: App<Element>) => {
 
   app
     .use(createPinia().use(piniaPluginPersistedstate))
-    .use(VueAxios, axios)
     .use(router)
     .use(vuetify);
   return app;
--- vite-project/src/plugins/vuetify.ts
+++ vite-project/src/plugins/vuetify.ts
@@ -2,13 +2,13 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { VDateInput } from 'vuetify/labs/VDateInput';
-import '@mdi/font/css/materialdesignicons.css';
+// import { VDateInput } from 'vuetify/labs/VDateInput';
+import "@mdi/font/css/materialdesignicons.css";
 
 export default createVuetify({
   components: {
     ...components,
-    VDateInput
+    // VDateInput
   },
   directives,
   icons: {
