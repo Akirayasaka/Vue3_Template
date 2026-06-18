d52170fb3607a7b19df2d0aff1d1a8e4e491a6b9
Akira <kuramal12@msn.com>
2026-06-18 09:54:04 +0800

    Update Packages

--- vite-project/src/types/interface/sample.ts
+++ vite-project/src/types/interface/sample.ts
@@ -1,8 +1,10 @@
+// 「不要」使用大寫 I 作為開頭，請寫 interface User，而不是 interface IUser。直接使用名詞本身 (PascalCase) 命名
+
 /** 範例用介面 */
 interface Sample {
-    id: number;
-    name: string;
-    description?: string; // Optional property
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  name: string;
+  description?: string; // Optional property
+  createdAt: Date;
+  updatedAt: Date;
 }
